fe241685030c327b8612653c01b8e757e1c3cbb0
il-pazzo <gcole@mac.com>
2017-08-30 15:38:51 -0700

    Updated app to show greeting, and POM to use java 8

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,9 +1,23 @@
 package clinic.programming.training;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
+    }
+
+    public void greet() {
+
+    	List<String> greetings = new ArrayList<>();
+	greetings.add( "Hello" );
+	greetings.add( "Howdy" );
+
+	for( String greeting: greetings ) {
+		p( "Greeting: " + greeting );
+	}
     }
 
     // method main(): ALWAYS the APPLICATION entry point
@@ -12,5 +26,7 @@ public class Application {
 
     	System.out.println ("Starting Application");
 	Application app = new Application();
+	app.greet();
     }
+    static void p( String s ) { System.out.println( s ); }
 }
