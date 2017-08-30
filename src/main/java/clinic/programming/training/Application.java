9d768b847531e82ead27c2f667d14491591934e3
il-pazzo <gcole@mac.com>
2017-08-30 16:39:34 -0700

    Added Apache Commons (StringUtils) to code and dependency to POM

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -3,10 +3,20 @@ package clinic.programming.training;
 import java.util.List;
 import java.util.ArrayList;
 
+import org.apache.commons.lang3.StringUtils;
+
+
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
+    }
+
+    public int countWords( String sentence ) {
+
+    	String[] words = StringUtils.split( sentence, ' ' );
+
+	return ( words == null ? 0 : words.length );
     }
 
     public void greet() {
@@ -27,6 +37,9 @@ public class Application {
     	System.out.println ("Starting Application");
 	Application app = new Application();
 	app.greet();
+
+	int numWords	= app.countWords( "I have four words" );
+	p( "Word count: " + numWords );
     }
     static void p( String s ) { System.out.println( s ); }
 }
